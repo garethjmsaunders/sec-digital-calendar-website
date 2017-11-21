67214b69f72c77f6590aa809053f4ebec5348f08
Unknown <gareth@garethjmsaunders.co.uk>
2017-11-21 21:24:30 +0000

    FORMAT (CSV lookup) Tidy
    Add PHPdoc style comment blocks.
    Tidy formating on spacing and comments.

--- php/csv-lookup.php
+++ php/csv-lookup.php
@@ -1,26 +1,63 @@
 <?php
 
-// TODO: Add phpdoc style comments and blocks
+/**
+ * CSV Lookup
+ * This script reads in the csv-lookup-calendar.csv file and parses
+ * it for useful information for the website homepage (only).
+ *
+ * @package     SEC digital calendar
+ * @subpackage  CSV lookup
+ * @version     1.2.0 2017-11-21
+ * @author      Gareth J M Saunders <gjms1@st-andrews.ac.uk>
+ * @license     http://opensource.org/licenses/gpl-license.php, GNU Public License
+ * @since       1.0.0
+ */
 
 
-    $updateDate    = 'Tuesday 21 November 2017';
-    $updateVersion = '2.2.0';
-    $filename      = './php/csv-lookup-calendar.csv';
 
-    // 2015-2016 iCalendar feed
-    $showOldIcalendar = 'https://calendar.google.com/calendar/ical/b2vd9e40nc9tk0gg460hojpkt4%40group.calendar.google.com/public/basic.ics';
-    // 2016-2017 iCalendar feed
-    $showNewIcalendar = 'https://calendar.google.com/calendar/ical/ujljc4nnc7nfilrmvffjarq30s%40group.calendar.google.com/public/basic.ics';
 
-// Open the CSV file, if it exists.
+/**
+ * Initiate variables.
+ *
+ * @version     1.1.0 2017-11-21
+ * @author      Gareth J M Saunders <gjms1@st-andrews.ac.uk>
+ * @license     http://opensource.org/licenses/gpl-license.php, GNU Public License
+ * @since       1.0.0
+ */
+
+$updateDate    = 'Tuesday 21 November 2017';
+$updateVersion = '2.2.0';
+$filename      = './php/csv-lookup-calendar.csv';
+
+// 2015-2016 iCalendar feed
+// $showOldIcalendar = 'https://calendar.google.com/calendar/ical/b2vd9e40nc9tk0gg460hojpkt4%40group.calendar.google.com/public/basic.ics';
+
+// 2016-2017 iCalendar feed
+$showOldIcalendar = 'https://calendar.google.com/calendar/ical/ujljc4nnc7nfilrmvffjarq30s%40group.calendar.google.com/public/basic.ics';
+
+// 2017-2018 iCalendar feed
+$showNewIcalendar = 'https://calendar.google.com/calendar/ical/5fdkf9c2lf1uh7rbb4ke8f3gc8%40group.calendar.google.com/public/basic.ics';
+
+
+
+
+/**
+ * Open the CSV file, if it exists.
+ *
+ * @version     1.0.0 2013-12-04
+ * @author      Gareth J M Saunders <gjms1@st-andrews.ac.uk>
+ * @license     http://opensource.org/licenses/gpl-license.php, GNU Public License
+ * @since       1.0.0
+ */
+
 if (($handle = fopen("$filename", "r")) !== FALSE)
 {
     // Set the parent multidimensional array key to 0.
     $nn = 0;
     while (($data = fgetcsv($handle, 0, ",")) !== FALSE) {
-        # Count the total keys in the row.
+        // Count the total keys in the row.
         $c = count($data);
-        # Populate the multidimensional array.
+        // Populate the multidimensional array.
         for ($x=0;$x<$c;$x++)
         {
             $csvarray[$nn][$x] = $data[$x];
@@ -28,31 +65,41 @@ if (($handle = fopen("$filename", "r")) !== FALSE)
         $nn++;
     }
 
-    // Close the File.
+    // Close the file.
     fclose($handle);
 } else {
     echo('CANNOT OPEN FILE');
 }
 
-// Take the row-ified data and columnize the array.
-// TODO: Why is there an else { nothing } ?!
-function columnizeArray($csvarray)
+
+
+
+/**
+ * FUNCTION: Take the row-ified data and columnize the array.
+ *
+ * @version     1.0.0 2013-12-04
+ * @author      Gareth J M Saunders <gjms1@st-andrews.ac.uk>
+ * @license     http://opensource.org/licenses/gpl-license.php, GNU Public License
+ * @since       0.1.0
+ */
+
+function columnizeArray( $csvarray )
 {
     $array = array();
-    foreach ($csvarray as $key=>$value)
+    foreach ( $csvarray as $key=>$value )
     {
         // Re-parse into useful array data.
-        if ($key == 0)
+        if ( $key === 0 )
         {
-            foreach ($value AS $key2=>$value2)
+            foreach ( $value as $key2=>$value2 )
             {
                 $array[$key2] = array();
                 $array[$key2][] = $value2;
             }
         }
-        else if ($key > 0)
+        else if ( $key > 0 )
         {
-            foreach ($value as $key3=>$value3)
+            foreach ( $value as $key3=>$value3 )
             {
                 $array[$key3][] = $value3;
             }
@@ -65,17 +112,31 @@ function columnizeArray($csvarray)
     return $array;
 }
 
-// TODO: Rename variables to make code easier to read and understand.
-// TODO: Cahgne $k == 0 to ===.
-function groupColumns($array = null) {
+
+
+
+/**
+ * FUNCTION: Look up information in the CSV array.
+ * Create a multidimensional array using date as the key.
+ * Columns: date,feast,colour,year,rcl,daily,showold,nextyear
+ *
+ * @version     1.0.0 2013-12-04
+ * @author      Gareth J M Saunders <gjms1@st-andrews.ac.uk>
+ * @license     http://opensource.org/licenses/gpl-license.php, GNU Public License
+ * @since       1.0.0
+*/
+
+// TODO: Change the variable names to make the code easier to read.
+
+function groupColumns( $array = null ) {
     $lookup = array();
-    foreach ($array as $k=>$v) {
+    foreach ( $array as $k=>$v ) {
         // process each column
         // $k = column number
         // $v = array of rows
-        if ($k == 0) {} else{ // working on column 2 or higher
+        if ( $k === 0 ) {} else { // working on column 2 or higher
             $lookup[$v[0]] = array();
-            foreach ($array[0] as $k1=>$v1) {
+            foreach ( $array[0] as $k1=>$v1 ) {
                 if ($v1 > 0) { // ignore the column heading
                     // Store the first column variable in as the key.
                     // Store the value associated with this item as the value.
@@ -83,12 +144,24 @@ function groupColumns($array = null) {
                     $lookup[$v[0]][$v1] = $v[$k1];
                 }
             }
-     }
+        }
     }
     return $lookup;
 }
 
-    $lookup = groupColumns(columnizeArray($csvarray));
+
+
+
+/**
+ * Look up today's date, feast, colour, and church years from the CSV file.
+ *
+ * @version     1.0.0 2013-12-04
+ * @author      Gareth J M Saunders <gjms1@st-andrews.ac.uk>
+ * @license     http://opensource.org/licenses/gpl-license.php, GNU Public License
+ * @since       1.0.0
+ */
+
+    $lookup = groupColumns( columnizeArray( $csvarray ) );
 
 //  Get today's date which is the lookup key for feast and colour
     $todaysDate = date("d/m/Y");
@@ -99,7 +172,7 @@ function groupColumns($array = null) {
 //  CSV column C - Lookup today's colour
     $todayColor = $lookup['colour'][$todaysDate];
 
-    switch($todayColor) {
+    switch( $todayColor ) {
         case 'green'  : $todayColorHex = '#3aa30b'; break;
         case 'red'    : $todayColorHex = '#a30b3a'; break;
         case 'violet' : $todayColorHex = '#750ba3'; break;
