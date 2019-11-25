dfd4718365833ceeb4aefb5e4e83009d2f2e5634
Gareth Saunders <gareth@garethjmsaunders.co.uk>
2019-11-25 21:57:51 +0000

    FEAT (Redesign) Add WIP redesign files

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # SEC digital calendar website
 
 * Version: 1.1.0
-* Last updated: Monday 25 November 2019
+* Last updated: Monday 21 November 2017
 
 <!-- MarkdownTOC -->
 
@@ -13,7 +13,6 @@
 
 ---
 
-<a id="1-about"></a>
 ## 1. About
 
 Each year I create a digital version of the [Scottish Episcopal Church calendar and lectionary](http://www.scotland.anglican.org/who-we-are/publications/liturgies/calendar-and-lectionary/) for importing into an electronic calendar, such as Microsoft Outlook, Google Calendar, Apple iCal, etc.
@@ -22,7 +21,6 @@ The website can be found at http://www.seccalendar.org.uk/.
 
 
 
-<a id="2-repositories"></a>
 ## 2. Repositories
 
 This repository contains the version-controlled code for the website.
@@ -30,7 +28,6 @@ This repository contains the version-controlled code for the website.
 The source files for the [SEC digital calendar](https://github.com/garethjmsaunders/sec-digital-calendar) can be found at https://github.com/garethjmsaunders/sec-digital-calendar.
 
 
-<a id="3-license"></a>
 ## 3. License
 
 This work is licensed under a [Creative Commons Attribution 4.0 International License](http://creativecommons.org/licenses/by/4.0/).
---
+++ cover.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>SEC digital calendar</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="The Revd Gareth J M Saunders">
+    <meta name="generator" content="Sublime Text 3">
+    <link rel="canonical" href="https://seccalendar.org.uk/">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="bootstrap/css/cover.css" rel="stylesheet">
+    <?php
+        error_reporting(0);
+        include('php/csv-lookup.php');
+        include('php/functions.php');
+    ?>
+  </head>
+  <body class="text-center">
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="masthead mb-auto">
+    <div class="inner">
+      <h3 class="masthead-brand">SEC digital calendar</h3>
+      <nav class="nav nav-masthead justify-content-center">
+        <a class="nav-link active" href="#">Home</a>
+        <a class="nav-link" href="#">Calendar</a>
+        <a class="nav-link" href="#">Subscribe</a>
+        <a class="nav-link" href="#">About</a>
+      </nav>
+    </div>
+  </header>
+
+  <main role="main" class="inner cover">
+    <h1 class="cover-heading">Cyril, Monk, 869, and Methodius, Bishop, 885</h1>
+    <p class="lead">'Apostles of the Slavs' <span class="badge badge-light badge-pill">6</span>
+      <br>White</p>
+    <h4><span class="badge badge-dark">Thursday 21 November 2019</span></h4>
+    <hr>
+    <p class="lead">Year C/1</p>
+  </main>
+
+  <footer class="mastfoot mt-auto">
+    <div class="inner">
+      <p class="lead"><a href="#" class="btn btn-lg btn-primary">Subscribe to the 2019–2020 calendar</a></p>
+    </div>
+  </footer>
+</div>
+</body>
+</html>
