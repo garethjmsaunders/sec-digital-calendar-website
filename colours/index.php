2d2d2a838e2ed5559ef4620d97660ed6e7fc6329
Gareth J M Saunders <garethjmsaunders@gmail.com>
2022-04-17 19:25:41 +0100

    FEAT (Liturgical Colours) WIP new page
    Add files for new liturgical colours page.

---
+++ colours/index.php
@@ -0,0 +1,229 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>SEC digital calendar | Liturgical colours</title>
+
+    <!-- meta -->
+    <link rel="canonical" href="https://seccalendar.org.uk/">
+    <meta name="author" content="The Revd Gareth J M Saunders">
+    <meta name="copyright" content="Copyright (c) <?php echo(date('Y')); ?> Gareth J M Saunders" />
+    <meta name="description" content="Scottish Episcopal Church digital calendar and lectionary in a digital format for importing into Google Calendar, Microsoft Outlook, etc">
+    <meta name="generator" content="Sublime Text 3">
+    <meta name="host" content="SiteGround">
+    <meta name="keywords" content="SEC, Scottish Episcopal Church, pisky, #pisky, calendar, kalendar, lectionary, saints, readings, festivals, Outlook, Google Calendar, iCal, iCalendar" />
+    <meta name="rating" content="general" />
+    <meta name="robots" content="index" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- favicon -->
+    <link rel="shortcut icon" href="../favicon.ico" />
+    <link rel="apple-touch-icon" href="../apple-touch-icon.png" />
+
+    <!-- CSS -->
+    <link href="../assets/css/bootstrap.min.css" type="text/css" rel="stylesheet">
+    <link href="../assets/css/style.css" type="text/css" rel="stylesheet">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-1866588-7"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-1866588-7');
+    </script>
+</head>
+
+<body class="banner damask damask-default text-center theme">
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <header class="masthead">
+            <div class="inner">
+                <h2 class="masthead-brand"><a href="../">SEC digital calendar</a></h2>
+                <nav class="nav nav-masthead justify-content-center">
+                    <a class="nav-link" href="../">Home</a>
+                    <a class="nav-link" href="../calendar/">Calendar</a>
+                    <a class="nav-link" href="../subscribe/">Subscribe</a>
+                    <a class="nav-link active" href="../about/">About</a>
+                </nav>
+            </div>
+        </header>
+
+        <main role="main" class="inner cover text-left">
+
+            <h1>Liturgical colours</h1>
+            <p class="lead">A brief guide to liturgical colours in the Scottish Episcopal Church</p>
+
+            <p>The church has defined a set of colours to be used within the context of Christian liturgy. These colours either offer a symbolism appropriate to a season of the liturgical year or they highlight a special occasion.</p>
+
+            <p>These colours may be used for vestments (tunicle, dalmatic and chasuble, copes, stoles, maniples and mitres), altar frontals, hangings, altar sets (burse, veil and pall), lectern bookmarks, etc.</p>
+
+            <p>The liturgical colours used within the Scottish Episcopal Church are <a href="#white">white or gold</a>, <a href="#red">red</a>, <a href="#green">green</a>, <a href="#violet">violet</a> or <a href="#lenten-array">Lenten array</a>, <a href="#rose">rose</a> and <a href="#black">black</a>; there is also a direction for no liturgical colour (<a href="#hangings-removed">hangings removed</a>).</p>
+
+
+            
+            <hr id="white" class="deep white">
+            <h3>White or gold</h3>
+            <p>Symbolism: celebration, exuberance, innocence, joy, purity, resurrection and victory</p>
+            <p><img src="../assets/img/damask-white.png" alt="white" width="50%"></p>
+            <p>White contains all the colours of the rainbow and is worn on major festivals and to commemorate saints who were not martyred. Vestments may be white, ivory, cream or silver, gold or yellow.</p>
+            <p>As a colour for funerals, white points to the hope of Easter. It is often used for the funeral of children and clergy.</p>
+            <p>When to use:</p>
+            <ul>
+                <li>Christmas <span class="rubric">(25 December to 5 January)</span></li>
+                <li>Feasts of the Lord
+                    <ul>
+                        <li>The Naming of Jesus <span class="rubric">(1 January)</span></li>
+                        <li>The Epiphany <span class="rubric">(6 January)</span></li>
+                        <li>The Baptism of the Lord <span class="rubric">(First Sunday after Epiphany)</span></li>
+                        <li>The Presentation of the Lord <span class="rubric">(2 February)</span></li>
+                        <li>The Annunciation of the Lord <span class="rubric">(25 March)</span></li>
+                        <li>Ascension Day <span class="rubric">(Thursday after the Sixth Sunday of Easter)</span></li>
+                        <li>The Transfiguration of the Lord <span class="rubric">(6 August)</span></li>
+                        <li>The Feast of Christ the King <span class="rubric">(The Last Sunday after Pentecost)</span></li>
+                    </ul>
+                </li>
+                <li>Maundy Thursday <span class="rubric">(Eucharist)</span></li>
+                <li>Easter <span class="rubric">(from Easter Vigil to Saturday after Seventh Sunday of Easter)</span></li>
+                <li>Trinity Sunday <span class="rubric">(Trinity Sunday to Saturday after Trinity Sunday)</span></li>
+                <li>Corpus Christi <span class="rubric">(Thanksgiving for Holy Communion, Thursday after Trinity Sunday)</span></li>
+                <li>Holy Cross Day <span class="rubric">(14 September)</span></li>
+                <li>Feasts of St Mary the Virgin
+                    <ul>
+                        <li>The Visit of St Mary to St Elizabeth <span class="rubric">31 May</span></li>
+                        <li>St Mary the Virgin <span class="rubric">(15 August)</span></li>
+                        <li>The Birth of St Mary <span class="rubric">(8 September)</span></li>
+                        <li>The Conception of St Mary <span class="rubric">(8 December)</span></li>
+                    </ul>
+                </li>
+                <li>Commemoration of Saints <span class="rubric">(including St John the Evangelist)</span></li>
+                <li>All Saints' Day <span class="rubric">(1 November)</span></li>
+                <li>All Souls' Day <span class="rubric">(2 November, if Black or Violet are not used)</span></li>
+                <li>Thanksgiving for Harvest</li>
+                <li>Dedication Festival</li>
+                <li>Funerals and requiem mass <span class="rubric">(if violet or black are not used)</span></li>
+            </ul>
+
+
+            <hr id="red" class="deep red">
+            <h3>Red</h3>
+            <p>Symbolism: Suffering of the Lord, Holy Spirit, commemoration of the Apostles and martyrs</p>
+            <p><img src="../assets/img/damask-red.png" alt="red" width="50%"></p>
+            <p>Red is the colour of fire and blood. Fire symbolises the Holy Spirit which came as tongues of fire at Pentecost (Acts 2). Representing blood, red symbolises Jesus's suffering during Holy Week, as well as the martyrs of the church.</p>
+            <p>When to use:</p>
+            <ul>
+                <li>Holy Week <span class="rubric">(if violet is not used)</span>
+                    <ul>
+                        <li>Palm Sunday <span class="rubric">The Sunday of the Passion, the beinning of Holy Week</span></li>
+                        <li>Monday of Holy Week</li>
+                        <li>Tuesday of Holy Week</li>
+                        <li>Wednesday of Holy Week</li>
+                        <li>Maundy Thursday <span class="rubric">(except at the Eucharist of the Last Supper)</span></li>
+                        <li>Good Friday <span class="rubric">(Holy Communion from the sacrament consecrated on Maundy Thursday, otherwise hangings removed)</span></li>
+                    </ul>
+                </li>
+                <li>Pentecost Sunday <span class="rubric">(Sunday after the Seventh Sunday of Easter)</span></li>
+                <li>Commemoration of apostles and evangelists <span class="rubric">(except St John)</span></li>
+                <li>Commemoration of martyrs</li>
+            </ul>
+
+
+            <hr id="green" class="deep green">
+            <h3>Green</h3>
+            <p>Symbolism: Everlasting life, fidelity (the neutral colour, the colour of nature) and hope</p>
+            <p><img src="../assets/img/damask-green.png" alt="green" width="50%"></p>
+            <p>Green is worn during what many parts of the Church refers to as 'ordinary time'. The colour of nature, green symbolises life and growth through each new day.</p>
+            <p>When to use:</p>
+            <ul>
+                <li>Epiphany season <span class="rubric">(from the Monday after the First Sunday after Epiphany to the Tuesday before Ash Wednesday)</span></li>
+                <li>Pentecost season <span class="rubric">(called Trinity in the Scottish Prayer Book, from the Monday after Trinity Sunday to the eve of the First Sunday of Advent)</span></li>
+            </ul>
+
+
+            <hr id="violet" class="deep violet">
+            <h3>Violet</h3>
+            <p>Symbolism: Sorrow, Mourning, Repentance, Penance.</p>
+            <p><img src="../assets/img/damask-violet.png" alt="violet" width="50%"></p>
+            <p>In the Scottish Episcopal Church a distinction is made between violet (liturgical colour) and purple (worn by bishops).</p>
+            <p>Violet is a penetential colour that represents the eschatological expectation of Advent as well as the preparation for the paschal mystery during Lent.</p>
+            <p>As a colour for funerals, violet references the hope of both Advent and Lent.</p>
+            <p>While blue is not specified in the Scottish Episcopal Church calendar, some churches may follow the Sarum rite which substitutes blue for violet during Advent.</p>
+            <p>When to use:</p>
+            <ul>
+                <li>Advent <span class="rubric">(from the First Sunday of Advent to Christmas Eve, if blue is not used)</span></li>
+                <li>Lent <span class="rubric">(from Ash Wednesday to the Eve of Palm Sunday, if Lenten array is not used)</span></li>
+                <li>Holy Week <span class="rubric">(if red is not used)</span>
+                    <ul>
+                        <li>Palm Sunday <span class="rubric">The Sunday of the Passion, the beinning of Holy Week</span></li>
+                        <li>Monday of Holy Week</li>
+                        <li>Tuesday of Holy Week</li>
+                        <li>Wednesday of Holy Week</li>
+                        <li>Maundy Thursday <span class="rubric">(except at the Eucharist of the Last Supper)</span></li>
+                        <li>Good Friday <span class="rubric">(Holy Communion from the sacrament consecrated on Maundy Thursday)</span></li>
+                    </ul>
+                </li>
+                <li>All Souls' Day <span class="rubric">(2 November, if Black or White are not used)</span></li>
+                <li>Penetential occasions</li>
+                <li>Funerals and requiem mass <span class="rubric">(if black or white are not used)</span></li>
+            </ul>
+
+
+            <hr id="lenten-array" class="deep lenten-array">
+            <h3>Lenten array</h3>
+            <p>Symbolism: mourning, penance, repentance, and sorrow.</p>
+            <p><img src="../assets/img/damask-lenten-array.png" alt="lenten array is unbleached ecru and scarlet" width="50%"></p>
+            <p>During Lent, an alternative to violet is Lenten array from the English Sarum rite.</p>
+            <p>The Sarum rite was developed at Salisbury Cathedral (Old Sarum) and used from the late 11th to the mid-16th century. Violet cloth would have been expensive to dye. So, the Sarum tradition used unbleached linen with crimson orphreys (ornamental stripes). This was sometimes referred to as 'Lenten white' or ash.</p>
+            <p>Symbolically, unbleached linen offers an absence of colour during Lent. Lenten Array also includes the veiling of the altar and reredos, statues, icons and crosses.</p>
+            <p>When to use:</p>
+            <ul>
+                <li>Lent <span class="rubric">(from Ash Wednesday to the Eve of Palm Sunday)</span></li>
+            </ul>
+
+
+            <hr id="rose" class="deep rose">
+            <h3>Rose</h3>
+            <p>Symbolism: Subdued joy</p>
+            <p><img src="../assets/img/damask-rose.png" alt="rose" width="50%"></p>
+            <p>Throughout both Advent and Lent, all Sundays are festivals. Each season includes one Sunday where the solemnity of the penetential season is lightened to one of subdued joy. This may be represented symbolically by replacing the liturgical colour of violet with rose.</p>
+            <p>During Advent, if an Advent wreath and candles is used, the third candle may be rose.</p>
+            <p>When to use:</p>
+            <ul>
+                <li>Gaudete Sunday <span class="rubric">(Third Sunday of Advent)</span></li>
+                <li>Laetare Sunday <span class="rubric">(Fourth Sunday of Lent)</span></li>
+            </ul>
+
+
+            <hr id="hangings-removed" class="deep hangings-removed">
+            <h3>Hangings removed</h3>
+            <p>Symbolism: absence, mourning, penance, repentance and sorrow.</p>
+            <p><img src="../assets/img/damask-hangings-removed.png" alt="rose" width="50%"></p>
+            <p>There is no liturgical colour suggested for Holy Saturday (the day between Good Friday and Easter Day). Instead, the words 'hangings removed' are printed in the lectionary and calendar.</p>
+            <p>Following the Eucharist of the Last Supper on Maundy Thursday, the altar and all decorations are removed from the sanctuary and any side chapels. The church building remains bare until the Easter Vigil, symbolising the stripping of Jesus before his crucifixion, death and lying in the tomb.</p>
+            <p>When to use:</p>
+            <ul>
+                <li>Triduum <span class="rubric">(from after the stripping of the altar on Maundy Thursday, through Good Friday to Holy Saturday)</span></li>
+            </ul>
+
+
+            <hr id="black" class="deep black">
+            <h3>Black</h3>
+            <p>Symbolism: death, grief and mourning</p>
+            <p><img src="../assets/img/damask-black.png" alt="black" width="50%"></p>
+            <p>Black has traditionally been the colour of mourning and grief.</p>
+            <p>When to use:</p>
+            <ul>
+                <li>All Souls’ Day <span class="rubric">(2 November, if violet or white are not used)</span></li>
+                <li>Funerals and requiem mass <span class="rubric">(if violet or white are not used)</span></li>
+            </ul>
+            <p>
+
+            
+        </main>
+
+        <footer>
+            <p><a href="../">Home</a> &middot; <a href="../calendar/">Calendar</a> &middot; <a href="../subscribe/">Subscribe</a> &middot; <a href="../about/">About</a></p>
+            <p>2004&ndash;<?php echo date("Y"); ?> &middot; <a href="https://www.facebook.com/groups/secoutlook/">Facebook</a> &middot; <a href="http://twitter.com/seccalendar">Twitter</a><br><a href="terms/">Terms and conditions</a></p>
+        </footer>
+    </div>
+</body>
+</html>
