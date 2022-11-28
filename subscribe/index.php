0cedaa390e61e07241996c68ce6c49733ef9ae4f
Gareth J M Saunders <garethjmsaunders@gmail.com>
2022-11-28 19:17:49 +0000

    FEAT (Subscribe) Update links
    Update subscribe and import/download links for 2022-2023 iCal and CSV

--- subscribe/index.php
+++ subscribe/index.php
@@ -59,8 +59,8 @@
             <!-- Perpetual -->
             <p><a href="https://calendar.google.com/calendar/ical/1u7edrnsjrmdtqs4k0n79sf6g0%40group.calendar.google.com/public/basic.ics" class="btn btn-primary">iCalendar feed .ics (auto updates annually)</a></p>
             
-            <!-- 2021-2022 -->
-            <p><a href="https://calendar.google.com/calendar/ical/t556cbppbia8ls1q9eh7ph0oss%40group.calendar.google.com/public/basic.ics" class="btn btn-secondary">iCalendar feed .ics (2021–2022)</a></p>
+            <!-- 2022-2023 -->
+            <p><a href="https://calendar.google.com/calendar/ical/cfc3e2db1a5ad3cf02d0b0ef07681f936fc9c4a57da0b46988441e9db345e429%40group.calendar.google.com/public/basic.ics" class="btn btn-secondary">iCalendar feed .ics (2022–2023)</a></p>
 
             <ul>
                 <li><a href="https://support.google.com/calendar/answer/37118?hl=en-GB">How to subscribe using Google Calendar</a></li>
@@ -72,8 +72,8 @@
 
             <h3>Import</h3>
             <p>Use this option if you wish to import a <tt>CSV</tt> file with the data into a new or existing calendar in Google Calendar, Microsoft Outlook or Apple iCal.</p>
-            <p><a href="https://www.seccalendar.org.uk/download/sec-master-calendar-2021-2022.csv" class="btn btn-secondary">Download .csv file (2021–2022)</a></p>
-            <p><a href="https://calendar.google.com/calendar/ical/t556cbppbia8ls1q9eh7ph0oss%40group.calendar.google.com/public/basic.ics" class="btn btn-secondary">Download .ics file (2021–2022)</a></p>
+            <p><a href="../download/sec-master-calendar-2022-2023.csv" class="btn btn-secondary">Download .csv file (2022–2023)</a></p>
+            <p><a href="https://calendar.google.com/calendar/ical/cfc3e2db1a5ad3cf02d0b0ef07681f936fc9c4a57da0b46988441e9db345e429%40group.calendar.google.com/public/basic.ics" class="btn btn-secondary">Download .ics file (2022–2023)</a></p>
             <ul>
                 <li><a href="https://support.google.com/calendar/answer/37118?hl=en-GB">How to import using Google Calendar</a></li>
                 <li><a href="https://support.office.com/en-gb/article/import-or-subscribe-to-a-calendar-in-outlook-com-cff1429c-5af6-41ec-a5b4-74f2c278e98c">How to import using Microsoft Outlook.com</a></li>
