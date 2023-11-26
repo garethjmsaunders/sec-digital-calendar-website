a8cd4218741fb4515fe9a0dcb737d79c378223e8
Gareth J M Saunders <garethjmsaunders@gmail.com>
2023-11-26 20:28:19 +0000

    FEAT (2023-2024) Subscription updates
    Add new CSV file to /download/.
    Update Subscribe page with new links to 2023-2024 resources.

--- subscribe/index.php
+++ subscribe/index.php
@@ -58,25 +58,29 @@
             <p><strong>Click the buttons below</strong> to copy the iCalendar feed URL to your clipboard.</p>
 
             <!-- Perpetual -->
-            <p><a href="https://calendar.google.com/calendar/ical/1u7edrnsjrmdtqs4k0n79sf6g0%40group.calendar.google.com/public/basic.ics" id="js-perpetual-btn" class="btn btn-primary">Copy SEC calendar (All years) .ics (auto updates annually)</a></p>
+            <p><a href="https://calendar.google.com/calendar/ical/1u7edrnsjrmdtqs4k0n79sf6g0%40group.calendar.google.com/public/basic.ics" id="js-perpetual-btn" class="btn btn-primary">Copy link to SEC calendar (All years) .ics (auto updates annually)</a></p>
             <p><textarea id="js-perpetual-textarea" class="w-100 text-bg-dark ical-feed" width="100%">https://calendar.google.com/calendar/ical/1u7edrnsjrmdtqs4k0n79sf6g0%40group.calendar.google.com/public/basic.ics</textarea></p>
             
-            <!-- 2022-2023 -->
-            <p><a href="https://calendar.google.com/calendar/ical/cfc3e2db1a5ad3cf02d0b0ef07681f936fc9c4a57da0b46988441e9db345e429%40group.calendar.google.com/public/basic.ics" id="js-year-btn" class="btn btn-secondary">Copy SEC calendar (2022–2023) .ics</a></p>
-            <p><textarea id="js-year-textarea" class="w-100 ical-feed text-bg-dark">https://calendar.google.com/calendar/ical/cfc3e2db1a5ad3cf02d0b0ef07681f936fc9c4a57da0b46988441e9db345e429%40group.calendar.google.com/public/basic.ics</textarea></p>
+            <!-- Update 1: 2023-2024 -->
+            <p><a href="https://calendar.google.com/calendar/ical/e551d88b9ac4a3bf31f7be97f345d19b6bde35c82e25306d1360861faf720e02%40group.calendar.google.com/public/basic.ics" id="js-year-btn" class="btn btn-secondary">Copy link to SEC calendar (2023–2024) .ics</a></p>
+            <p><textarea id="js-year-textarea" class="w-100 ical-feed text-bg-dark">https://calendar.google.com/calendar/ical/e551d88b9ac4a3bf31f7be97f345d19b6bde35c82e25306d1360861faf720e02%40group.calendar.google.com/public/basic.ics</textarea></p>
 
             <ul>
                 <li><a href="https://support.google.com/calendar/answer/37118?hl=en-GB">How to subscribe using Google Calendar</a></li>
                 <li><a href="https://support.google.com/calendar/answer/37100?hl=en&co=GENIE.Platform%3DAndroid">How to subscribe using Android</a></li>
                 <li><a href="https://support.office.com/en-gb/article/import-or-subscribe-to-a-calendar-in-outlook-com-cff1429c-5af6-41ec-a5b4-74f2c278e98c">How to subscribe using Microsoft Outlook.com</a></li>
                 <li><a href="https://support.apple.com/en-gb/guide/calendar/icl1022/mac">How to subscribe using Apple Calendar</a></li>
-                <li><a href="https://support.apple.com/en-us/HT202361">How to subscribe using Apple iPhone</a></li>
+                <li><a href="https://support.apple.com/en-us/HT202361">How to subscribe using Apple iPhone, iPad and iPod Touch</a></li>
             </ul>
 
             <h3>Import</h3>
             <p>Use this option if you wish to import a <tt>CSV</tt> or <tt>iCalendar</tt> file with the data into a new or existing calendar in Google Calendar, Microsoft Outlook, Apple Calendar, GNOME Calendar or any other compatible calendar application that supports iCalendar files or importing CSV files..</p>
-            <p><a href="../download/sec-master-calendar-2022-2023.csv" class="btn btn-secondary">Download Comma Separated Values (*.csv) file (2022–2023)</a></p>
-            <p><a href="https://calendar.google.com/calendar/ical/cfc3e2db1a5ad3cf02d0b0ef07681f936fc9c4a57da0b46988441e9db345e429%40group.calendar.google.com/public/basic.ics" class="btn btn-secondary">Download iCalendar (*.ics) file (2022–2023)</a></p>
+
+            <!-- Update 2: CSV download. Upload file to /download/ and update this link and JS below -->
+            <p><a href="../download/sec-master-calendar-2023-2024.csv" class="btn btn-secondary">Download Comma Separated Values (*.csv) file (2023–2024)</a></p>
+
+            <!-- Update 3: iCalendar URL -->
+            <p><a href="https://calendar.google.com/calendar/ical/e551d88b9ac4a3bf31f7be97f345d19b6bde35c82e25306d1360861faf720e02%40group.calendar.google.com/public/basic.ics" class="btn btn-secondary">Download iCalendar (*.ics) file (2023–2024)</a></p>
             <ul>
                 <li><a href="https://support.google.com/calendar/answer/37118?hl=en-GB">How to import using Google Calendar</a></li>
                 <li><a href="https://support.office.com/en-gb/article/import-or-subscribe-to-a-calendar-in-outlook-com-cff1429c-5af6-41ec-a5b4-74f2c278e98c">How to import using Microsoft Outlook.com</a></li>
@@ -127,7 +131,7 @@
                 
                 // Copy to clipboard
                 navigator.clipboard.writeText(copyText.value).then(() => {
-                    alert("SEC calendar (2022-2023) iCalendar (*.ics) link address copied to clipboard");
+                    alert("SEC calendar (2023-2024) iCalendar (*.ics) link address copied to clipboard");
                 });
             });
 
